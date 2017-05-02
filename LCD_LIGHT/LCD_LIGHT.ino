fe3656d06b98bb0aca3501fcaafe30004ae7cd57
pyjun1103 <kukluxklan0@naver.com>
2017-05-02 19:56:52 +0900

    Add files via upload

---
+++ LCD_LIGHT/LCD_LIGHT.ino
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal myLCD(7,8,9,10,11,12);
+
+void setup() {
+  myLCD.begin(16,2); // 16x2 LCD will used by me
+  myLCD.clear(); // LCD display clear
+  Serial.begin(9600);
+}
+
+int count = 0;
+void loop() {
+  myLCD.clear();
+  int light = analogRead(A5); // 0~1023, through light amount
+
+  myLCD.setCursor(0,0);
+  myLCD.print("Current Light");
+  myLCD.setCursor(0,1);
+  myLCD.print(light);
+
+  delay(1000);
+}
